8ef66b5bfae5f43443cf2a17bec4dba7a6a2f2b3
Bob Yantosca <yantosca@seas.harvard.edu>
2012-04-30 15:06:42 -0400

    Modifications for compatibility with netCDF-3
    (1) Makefile_header.mk: Now allow the user to compile
        with the option NETCDF3=yes, which will set the
        proper include and library paths.  The default is
        to assume that the user is compiling with netCDF-4.
        Also updated comments
    
    (2) Makefile: Added comments about the new NETCDF3=yes
        makefile option.
    
    (3) m_netcdf_io_readattr.F90: Now call the proper netCDF
        library functions for 100% compatibility with the
        older netCDF-3, as well as for with netCDF-4.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- Code/Makefile_header.mk
+++ Code/Makefile_header.mk
@@ -12,23 +12,35 @@
 #\\
 #\\
 # !REMARKS:
-# To build the programs, call "make" with the following syntax:
-#
-#   make TARGET [ OPTIONAL-FLAGS ]
-#
-# To display a complete list of options, type "make help".
-#
-# The following variables are exported to the main-level Makefile:
-#
-# Variable   Description
-# --------   -----------
-# F90        Contains the Fortran compilation commands
-# FREEFORM   Contains the command to force F90 "free format" compilation
-# LD         Contains the command to link to libraries & make executable
-# LINK_NC    Specifies the command to link to the HDF libraries on this system
-#
-# FFLAGS, DIR_HDF, LINK_NC are local variables that are not returned 
-# to the "outside world".
+#  To build the programs, call "make" with the following syntax:
+#                                                                             .
+#    make TARGET [ OPTIONAL-FLAGS ]
+#                                                                             .
+#  To display a complete list of options, type "make help".
+#                                                                             .
+#  NOTE: The NcdfUtilities assumes that you are linking to a netCDF-4 
+#  library by default.  If you are linking to a netCDF-3 library build,
+#  then compile with the flag NETCDF3=yes.
+#                                                                             .
+#  The following variables are accepted either as command-line options,
+#  or may be defined in your ~/.cshrc or ~/.bashrc file:
+#                                                                             .
+#  Variable     Description
+#  ----------   -----------
+#  INC_NETCDF   Specifies the netCDF include path (for netCDF-3 or netCDF-4)
+#  LIB_NETCDF   Specifies the netCDF library path (for netCDF-3 or netCDF-4)
+#  INC_HDF5     Specifies the HDF-5  library path (only needed for netCDF-4)
+#  LIB_HDF5     Specifies the HDF-5  include path (only needed for netCDF-4)
+#  LIB_ZLIB     Specifies the ZLIB   include path (only needed for netCDF-4)
+#                                                                             .
+#  The following variables are exported to the main-level Makefile:
+#                                                                             .
+#  Variable     Description
+#  ----------   -----------
+#  F90          Contains the Fortran compilation commands
+#  FREEFORM     Contains the command to force F90 "free format" compilation
+#  LD           Contains the command to link to libraries & make executable
+#  LINK_NC      Specifies the command to link to the netCDF libraries
 #
 # !REVISION HISTORY: 
 #  04 Aug 2009 - R. Yantosca - Initial version
@@ -37,6 +49,9 @@
 #  24 Jan 2012 - R. Yantosca - ifort is now the default compiler.  We will
 #                              invoke mpif90 at a later date.
 #  30 Jan 2012 - R. Yantosca - Now create/read modules in the $MOD directory
+#  30 Apr 2012 - R. Yantosca - Now set include & link paths 
+#  30 Apr 2012 - R. Yantosca - Now use -mcmodel=medium option for IFORT, PGI
+#  30 Apr 2012 - R. Yantosca - 
 #EOP
 #------------------------------------------------------------------------------
 #BOC
@@ -47,22 +62,22 @@
 
 # Make ifort the default compiler
 ifndef COMPILER
-COMPILER = ifort
+COMPILER := ifort
 endif
 
-###############################################################################
-# Include directory for NETCDF library
-# Modify this accordingly for your system!
-INC_NC  = -I$(BL_INC_NETCDF) -I$(BL_INC_HDF5)
-###############################################################################
-# Library link commands for NETCDF library
-# Modify this accordingly for your system!
-LINK_NC = \
--L$(BL_LIB_NETCDF) -lnetcdf \
--L$(BL_LIB_HDF5) -lhdf5_hl \
--L$(BL_LIB_HDF5) -lhdf5 \
--L$(BL_LIB_ZLIB) -lz
-###############################################################################
+# Pick the include & library link paths depending on whether we are using a 
+# build of netCDF-3 or netCDF-4.  netCDF-4 also requires installation of the
+# HDF5 and ZLIB libraries.  
+ifeq ($(NETCDF3),yes)
+INC_NC  := -I$(INC_NETCDF)
+LINK_NC := -L$(LIB_NETCDF) -lnetcdf
+else
+INC_NC  := -I$(INC_NETCDF) -I$(INC_HDF5)
+LINK_NC := -L$(LIB_NETCDF) -lnetcdf \
+           -L$(LIB_HDF5) -lhdf5_hl \
+           -L$(LIB_HDF5) -lhdf5 \
+           -L$(LIB_ZLIB) -lz
+endif
 
 #==============================================================================
 # IFORT compilation options (default)
@@ -71,7 +86,7 @@ ifeq ($(COMPILER),ifort)
 
 # Pick correct options for debug run or regular run 
 ifdef DEBUG
-FFLAGS = -cpp -w -noalign -convert big_endian -g -traceback 
+FFLAGS = -cpp -w -noalign -convert big_endian -g -traceback -mcmodel=medium
 else
 FFLAGS = -cpp -w -O2 -auto -noalign -convert big_endian -openmp -mcmodel=medium
 endif
@@ -102,9 +117,9 @@ ifeq ($(COMPILER),mpif90)
 
 # Pick correct options for debug run or regular run 
 ifdef DEBUG
-FFLAGS = -cpp -w -noalign -convert big_endian -g -traceback 
+FFLAGS = -cpp -w -noalign -convert big_endian -g -traceback -mcmodel=medium
 else
-FFLAGS = -cpp -w -O2 -auto -noalign -convert big_endian -openmp
+FFLAGS = -cpp -w -O2 -auto -noalign -convert big_endian -openmp -mcmodel=medium
 endif
 
 # Add option for "array out of bounds" checking
@@ -133,9 +148,9 @@ ifeq ($(COMPILER),pgi)
 
 # Pick correct options for debug run or regular run 
 ifdef DEBUG
-FFLAGS = -byteswapio -Mpreprocess -fast -Bstatic
+FFLAGS = -byteswapio -Mpreprocess -fast -Bstatic -mcmodel=medium
 else
-FFLAGS = -byteswapio -Mpreprocess -fast -mp -Mnosgimp -DHE4 -Bstatic
+FFLAGS = -byteswapio -Mpreprocess -fast -mp -Mnosgimp -DHE4 -Bstatic -mcmodel=medium
 endif
 
 # Add option for "array out of bounds" checking
